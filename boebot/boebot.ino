abedb012082ba7a629b42bf6b62779c203e1b4ba
Pavel Pochobradsky <pavlik.pocho@gmail.com>
2022-03-31 12:32:24 +0100

    Final code

--- boebot/boebot.ino
+++ boebot/boebot.ino
@@ -1,226 +1,281 @@
-// Robotics with the BOE Shield - MovementsWithSimpleFunctions
-// Move forward, left, right, then backward for testing and tuning.
+
+// NOTE: THERE ARE THREE THINGS TO DO AND CHECK IN HERE!!
+
 #include <Arduino.h>
 #include <Servo.h> // Include servo library
-//#include <SoftwareSerial.h>
- 
-Servo servoLeft; // Declare left and right servos
+// #include <SoftwareSerial.h> // For when software serial is used
+
+// Number of unique data points transmitted from remote control
+#define N_IDENTIFIERS 7
+
+// Declare left, right and forklift servos
+Servo servoLeft;
 Servo servoRight;
 Servo servoForklift;
 
-bool a = false;
-bool b = false;
-bool c = false;
-bool d = false;
-bool e = false;
+// All arrays initialized to [.., 0, ..] because they are global
 
-uint8_t x0 = 0;
-uint8_t x1 = 0;
+// availChecks are used to record which datapoint will come next
+// so that we can loop around and check if the datapoint
+// has already arrived. Without this, we would be reading
+// things that haven't arrived yet.
+bool availChecks[N_IDENTIFIERS];
 
-uint8_t y0 = 0;
-uint8_t y1 = 0;
+// dataInit keeps track of which datapoints were already updated
+// so that the robot doesn't do anything crazy when being turned on
+// until useful data has come through from the remote
+bool dataInit[N_IDENTIFIERS];
 
-uint8_t btn0 = 0;
+// Once everything is set, save it to speed up later checks
+bool allSet;
+
+// someAvailable just helps to not check for new identifiers when
+// one is already available
+bool someAvailable = false;
+
+// identifiers contains incoming datapoint identifiers (a,b,c...)
+char identifiers[N_IDENTIFIERS] = {'a', 'b', 'c', 'd', 'e', 'f', 'g'};
+
+// vals saves the incoming values as they are read
+// they are then redistributed into the right variables later
+byte vals[N_IDENTIFIERS];
+
+// 
+uint8_t rawButton0 = 0;
+uint8_t rawButton1 = 0;
+uint8_t rawButton2 = 0;
+int16_t rawX = 512;
+int16_t rawY = 512;
 
 int16_t x = 512;
 int16_t y = 512;
 
-int16_t finalX = 512;
-int16_t finalY = 512;
+uint8_t button0 = 0;
+bool prevButton0Pressed = false;
+bool button0Pressed = false;
+uint8_t button1 = 0;
+bool button1Pressed = false;
+uint8_t button2 = 0;
+bool button2Pressed = false;
 
-int16_t offsetX = 0;
-int16_t offsetY = 0;
+uint8_t topStopSwitchPin = 2;
+uint8_t bottomStopSwitchPin = 3;
+uint8_t topStopSwitch = 0;
+uint8_t bottomStopSwitch = 0;
 
-uint8_t finalBtnVal = 0;
-bool buttonPressed = false;
+bool precisionMode = false;
 
-bool forkliftDir = false;
-unsigned long stopForkliftAt = 0;
+// SoftwareSerial for testing if hardware serial needs to be used for logging
+// It's not fast enough for actual usage, also uses pins we need
+// SoftwareSerial btSerial = SoftwareSerial(2, 3);
 
-//SoftwareSerial btSerial = SoftwareSerial(2, 3);
- 
-void setup() // Built-in initialization block
+// Setup everything that needs to happen once
+void setup()
 { 
-  servoLeft.attach(13); // Attach left signal to pin 13
-  servoRight.attach(12); // Attach right signal to pin 12
-  servoForklift.attach(11); // Is forklift on pin 11? Probably? okee
+  // Set the stop switch pins to input
+  pinMode(topStopSwitchPin, INPUT);
+  pinMode(bottomStopSwitchPin, INPUT);
+  
+  // Attach servos
+  servoForklift.attach(11); // Attach forklift signal to pin 11
+  servoRight.attach(12); // Right signal to pin 12
+  servoLeft.attach(13); // Left signal - pin 13
+  
+  // Initialize forklift servo to 1495, which is the mid-value and makes it stop
   servoForklift.writeMicroseconds(1495);
+  
+  // Start the serial comms
   Serial.begin(115200);
-//  btSerial.begin(115200);
+  // btSerial.begin(115200);
   
 } 
- 
-void loop() // Main loop auto-repeats
+
+// This literally runs in circles as fast as it can
+void loop()
 {
   
-//  while (Serial.available()) {
-//    delay(1);
-//    btSerial.write(Serial.read());
-//  }
-//  delay(500);
-//  while (btSerial.available()) {
-//    Serial.write(btSerial.read());
-//  }
-  if (Serial.available()) {
-    while (Serial.available()) {
-      if (a) {
-        x0 = Serial.read();
-        x = x0 | (x1 << 8);
-        a = false;
-      }
-      if (b) {
-        x1 = Serial.read();
-        x = x0 | (x1 << 8);
-        b = false;
-      }
-      if (c) {
-        y0 = Serial.read();
-        y = y0 | (y1 << 8);
-        c = false;
-      }
-      if (d) {
-        y1 = Serial.read();
-        y = y0 | (y1 << 8);
-        d = false;
-      }
-      if (e) {
-        btn0 = Serial.read();
-        e = false;
-      }
-      if (Serial.peek() == 'a') {
-        Serial.read();
-        a = true;
-        b = false;
-        c = false;
-        d = false;
-        e = false;
-      }
-      else if (Serial.peek() == 'b') {
-        Serial.read();
-        b = true;
-        a = false;
-        c = false;
-        d = false;
-        e = false;
-      }
-      else if (Serial.peek() == 'c') {
-        Serial.read();
-        c = true;
-        b = false;
-        a = false;
-        d = false;
-        e = false;
-      }
-      else if (Serial.peek() == 'd') {
-        Serial.read();
-        d = true;
-        b = false;
-        c = false;
-        a = false;
-        e = false;
-      }
-      else if (Serial.peek() == 'e') {
-        Serial.read();
-        d = false;
-        b = false;
-        c = false;
-        a = false;
-        e = true;
-      }
-      else {
-        Serial.read();
-      }
-    }
+  // Read the incoming Bluetooth data
+  readBTData();
 
-    // Button:
+  // If not all data is initialized, return and wait for more data
+  if (notAllDataIsInitialized()) return;
 
-    finalBtnVal = (uint8_t)((float)(finalBtnVal) * 0.9f + (float)btn0 * 0.1f);
-    buttonPressed = finalBtnVal < 128;
+  // Save the raw data into specific global variables
+  saveRawData();
 
-    // CAUTION: Stationary point for the forklift servo is 1495!
+  // ------------------------------------------------------------------
+  // At this point, all data from the remote must be up-to-date
+  // ------------------------------------------------------------------
+  
+  // Update all button values to see if they are pressed
+  updateButtonValues();
 
-    // Replace this with actual logic
-    if (buttonPressed && stopForkliftAt < millis()) {
-      stopForkliftAt = millis() + 2000;
-      forkliftDir = !forkliftDir;
-    }
-    if (stopForkliftAt > millis()) {
-      if (forkliftDir) {
-        servoForklift.writeMicroseconds(1600); 
-      }
-      else {
-        servoForklift.writeMicroseconds(1400); 
-      }
+  // Update the precision mode
+  updatePrecisionMode();
+
+  // Update the stop switch values 
+  getStopSwitchValues();
+
+  // Update the forklift servo based on button values
+  controlForklift();
+
+  // Update joystick values
+  updateJoystickValues();
+
+  // Update the motor servos based on joystick value
+  controlMotors();
+  
+}
+
+void readBTData() {
+
+  // The loop goes while serial data is available
+  // Each time, we first check if the last transmission
+  // was some identifier and we should expect a value to come in
+  
+  // Otherwise, we check if the next thing coming in is an
+  // identifier
+
+  // The purpose of this is to always check if data is available
+  // before attempting to read it
+
+  while (Serial.available()) {
+    if (someAvailable) {
+      // Some data should have come, check what it is
+      for (uint8_t i = 0; i < N_IDENTIFIERS; i++) {
+        if (availChecks[i]) {
+          // Save data, reset the checks and confirm data as initialized
+          vals[i] = Serial.read();
+          availChecks[i] = false;
+          someAvailable = false;
+          dataInit[i] = true;
+        }
+      } 
     }
     else {
-      servoForklift.writeMicroseconds(1495);
+      // No data expected, check for next identifier
+      char n_id = Serial.read();
+      for (uint8_t i = 0; i < N_IDENTIFIERS; i++) {
+        if (n_id == identifiers[i]) {
+          // Remember which data is about to come in
+          availChecks[i] = true;
+          someAvailable = true;
+        }
+      }
     }
-    
-
-    // Need to map it so that 1000 is 1600 and 0 is 1400 for Y
-    // 1000 x means one is 1600, one is 1400
-
-    if (x < 0 || x > 1024 || y < 0 || y > 1024) {
-      return;
-    }
-    
-    finalX = (int16_t)((float)finalX * 0.5f + (float)x * 0.5f);
-    finalY = (int16_t)((float)finalY * 0.5f + (float)y * 0.5f);
-    
-    if (finalX >= 496 && finalX <= 525) {
-      finalX = 512;
-    }
-    if (finalY >= 496 && finalY <= 525) {
-      finalY = 512;
-    }
-
-    float xF = (float)finalX;
-    float yF = (float)finalY;
-
-    float throttle = (float)(yF) / 1024.0f * 200.0f - 100.0f;
-    float steering = (float)(xF) / 1024.0f * 200.0f - 100.0f;
-
-    servoLeft.writeMicroseconds(max(1400, min(1600, 1500 + throttle + steering)));
-    servoRight.writeMicroseconds(max(1400, min(1600, 1500 + throttle - steering)));
-    
   }
   
 }
 
-// Original moving functions
- 
-void forward(int time) // Forward function
-{ 
- servoLeft.writeMicroseconds(1550); // Left wheel counterclockwise
- servoRight.writeMicroseconds(1450); // Right wheel clockwise
- delay(time); // Maneuver for time ms
- servoLeft.writeMicroseconds(2000); // Left wheel counterclockwise
- servoRight.writeMicroseconds(1000); // Right wheel clockwise
- delay(time); // Maneuver for time ms
-} 
-void turnLeft(int time) // Left turn function
-{ 
- servoLeft.writeMicroseconds(1000); // Left wheel clockwise
- servoRight.writeMicroseconds(1000); // Right wheel clockwise
- delay(time); // Maneuver for time ms
-} 
-void turnRight(int time) // Right turn function
-{ 
- servoLeft.writeMicroseconds(2000); // Left wheel counterclockwise
- servoRight.writeMicroseconds(2000); // Right wheel counterclockwise
- delay(time); // Maneuver for time ms
-} 
-void backward(int time) // Backward function
-{ 
- servoLeft.writeMicroseconds(1450); // Left wheel clockwise
- servoRight.writeMicroseconds(1550); // Right wheel counterclockwise
- delay(time); // Maneuver for time ms
- servoLeft.writeMicroseconds(1000); // Left wheel clockwise
- servoRight.writeMicroseconds(2000); // Right wheel counterclockwise
- delay(time); // Maneuver for time ms
-} 
-void disableServos() // Halt servo signals
-{ 
- servoLeft.detach(); // Stop sending servo signals
- servoRight.detach();
-} 
+bool notAllDataIsInitialized() {
+  
+  // Check if all data has been updated at least once
+  // If it hasn't, indicate this so that loop can go again
+  if (!allSet) {
+    bool breakOut = false;
+    for (uint8_t i = 0; i < N_IDENTIFIERS; i++) {
+      if (!dataInit[i]) {
+        breakOut = true;
+        break;
+      }
+    }
+    // If everything is set, remember this, so that we
+    // don't need to check again
+    if (!breakOut) { allSet = true; }
+    return breakOut;
+  }
+  return false;
+  
+}
+
+void saveRawData() {
+  
+  // Set the specific variables from the vals array
+  // What goes where is determined on the remote side
+  rawX = vals[0] | (vals[1] << 8);
+  rawY = vals[2] | (vals[3] << 8);
+  rawButton0 = vals[4];
+  rawButton1 = vals[5];
+  rawButton2 = vals[6];
+  
+}
+
+void updateButtonValues() {
+  
+  // Update button values using memory efficient low-pass filters, such
+  // that noise does trigger them accidentally
+  // except for button0, thats handeled on the remote
+  button0 = rawButton0;
+  button1 = (uint8_t)((float)(button1) * 0.9f + (float)rawButton1 * 0.1f);
+  button2 = (uint8_t)((float)(button2) * 0.9f + (float)rawButton2 * 0.1f);
+  
+  // Save the booleans of whether each button was pressed and also if button0 was
+  // previously pressed
+  prevButton0Pressed = button0Pressed;
+  button0Pressed = button0 >= 128;
+  button1Pressed = button1 >= 128;
+  button2Pressed = button2 >= 128;
+  
+}
+
+void updatePrecisionMode() {
+
+  // If button0 was previously not pressed, but now is, change precision mode
+  precisionMode = (button0Pressed && !prevButton0Pressed) ? !precisionMode : precisionMode;
+  
+}
+
+void getStopSwitchValues() {
+
+  // Set the digital stop switch values
+  topStopSwitch = digitalRead(topStopSwitchPin);
+  bottomStopSwitch = digitalRead(bottomStopSwitchPin);
+  
+}
+
+void controlForklift() {
+
+  // Check which button is pressed and if stop switches are activated
+  if (button2Pressed && !button1Pressed && !topStopSwitch) {
+    servoForklift.writeMicroseconds(1550);
+  }
+  else if (button1Pressed && !button2Pressed && !bottomStopSwitch) {
+    servoForklift.writeMicroseconds(1440);
+  }
+  else {
+    // Stationary point for the forklift servo is 1495
+    servoForklift.writeMicroseconds(1495);
+  }
+  
+}
+
+void updateJoystickValues() {
+
+  // Ignore this dataset if any value if out of bounds
+  if (rawX < 0 || rawX > 1024 || rawY < 0 || rawY > 1024) return;
+
+  // Update reasonable joystick values using memory-efficient low-pass filters
+  // This is less agressive because this data is already filtered
+  // on the remote side
+  x = (int16_t)((float)x * 0.5f + (float)rawX * 0.5f);
+  y = (int16_t)((float)y * 0.5f + (float)rawY * 0.5f);
+
+  // If values are close to mid-point, treat them as if they
+  // were exactly in the middle. This creates a dead zone, but makes
+  // sure that if the joystick is released, the robot stops
+  if (x >= 490 && x <= 534) x = 512;
+  if (y >= 490 && y <= 534) y = 512;
+  
+}
+
+void controlMotors() {
+
+  // Convert x and y joystick data into throttle and steering input
+  float throttle = (float)(x) / 1024.0f * 200.0f - 100.0f;
+  float steering = (float)(y) / 1024.0f * 200.0f - 100.0f;
+
+  // Set the power level of the servo motors according to throttle and steering values
+  servoLeft.writeMicroseconds(max(1400, min(1600, 1500 + ((throttle - steering) * (precisionMode ? 0.3f : 1.0f)))));
+  servoRight.writeMicroseconds(max(1400, min(1600, 1500 + ((throttle + steering) * (precisionMode ? 0.3f : 1.0f)))));
+
+}
--- boebot_remote/boebot_remote.ino
+++ boebot_remote/boebot_remote.ino
@@ -1,75 +1,182 @@
-// Robotics with the BOE Shield - MovementsWithSimpleFunctions
-// Move forward, left, right, then backward for testing and tuning.
-//#include <AltSoftSerial.h>
 #include <Arduino.h>
+// #include <AltSoftSerial.h> // For when software serial is used
 
-unsigned long lastSend = 0;
+// Number of unique data points transmitted from remote control
+#define N_IDENTIFIERS 7
+
+// sendInterval is how often (in ms) Bluetooth data is sent
 unsigned long sendInterval = 25;
 
-float calibrationDoneF = 0.0f;
+// Keep track of the last time Bluetooth data was sent
+unsigned long lastSend = 0;
 
-int16_t x = 512;
-int16_t y = 512;
-int btn = 0;
+// This number is decremented on each cycle and when it's 0,
+// the system is assumed to have settled and the offset of the
+// joystick read is saved (we assume the joystick is at middle position)
+uint8_t cyclesUntilCalibration = 100;
 
-int16_t finalX = 512;
-int16_t finalY = 512;
+// identifiers saves the ids that are sent before each
+// data point
+char identifiers[N_IDENTIFIERS] = {'a', 'b', 'c', 'd', 'e', 'f', 'g'};
 
+// Raw (no calibration applied) joystick values need to be stored
+// due to design
+int16_t rawJoystickX = 512;
+int16_t rawJoystickY = 512;
+int16_t button0analog = 512;
+
+// Button values
+byte button0 = 0;
+byte button1 = 0;
+byte button2 = 0;
+
+// Joystick values after calibration offset applied
+int16_t joystickX = 512;
+int16_t joystickY = 512;
+
+// Joystick calibration offsets
 int16_t offsetX = 0;
 int16_t offsetY = 0;
 
-//AltSoftSerial btSerial;
+// SoftwareSerial for testing if hardware serial needs to be used for logging
+// It's not fast enough for actual usage, also uses pins we need
+// AltSoftSerial btSerial;
  
 void setup()
-{  
+{
+  // Set pinmodes
+  pinMode(2, INPUT);
+  pinMode(3, INPUT);
+  
+  // Start serial comms
   Serial.begin(115200);
-//  btSerial.begin(115200);
+  // btSerial.begin(115200);
   
 } 
- 
-void loop() // Main loop auto-repeats
+
+
+void loop()
 {
 
-  if (lastSend + sendInterval < millis()) {
-    lastSend = millis();
+  // Check if enough time has elapsed since the last
+  // time data was sent
+  if (lastSend + sendInterval >= millis()) return;
+  lastSend = millis();
 
-    if (calibrationDoneF < 1.0f) {
-      offsetX = x - 512;
-      offsetY = y - 512;
-      calibrationDoneF += 0.01f;
-    }
-    
-    x = (int16_t)((float)x * 0.75f + (float)analogRead(A0) * 0.25f);
-    y = (int16_t)((float)y * 0.75f + (float)analogRead(A1) * 0.25f);
-    btn = analogRead(A2) >= 50 ? 255 : 0;
+  // Wait for 100 loops until we record the joystick
+  // reading offset, don't continue if not calibrated
+  if (!doAndCheckIfCalibrated) return;
 
-    finalX = x - offsetX;
-    finalY = y - offsetY;
+  // Retrieve the joystick values and clean them up
+  getCalibratedJoystickValues();
 
-    if (finalX >= 496 && finalX <= 525) {
-      finalX = 512;
-    }
-    if (finalY >= 496 && finalY <= 525) {
-      finalY = 512;
-    }
-    
-    byte x0 = (byte)(finalX);
-    byte x1 = (byte)(finalX >> 8);
-    
-    byte y0 = (byte)(finalY);
-    byte y1 = (byte)(finalY >> 8);
+  // Check if we should treat the joystick position as centered
+  checkForJoystickDeadspace();
 
-    Serial.print('a');
-    Serial.write(x0);
-    Serial.print('b');
-    Serial.write(x1);
-    Serial.print('c');
-    Serial.write(y0);
-    Serial.print('d');
-    Serial.write(y1);
-    Serial.print('e');
-    Serial.write(btn);
-    Serial.println("");    
+  // Get the button values as a digital signal
+  getButtonValues();
+
+  // Combine the data into an array and send
+  combineAndSendData();
+  
+}
+
+bool doAndCheckIfCalibrated() {
+
+  // Check if we are still waiting for calibration
+  // While we wait, we return false to indicate the
+  // system isn't calibrated
+  // Also calibrate the system if it's time
+  
+  if (cyclesUntilCalibration > 0) {
+    cyclesUntilCalibration -= 1;
+    return false;
   }
+  else if (cyclesUntilCalibration == 0) {
+    offsetX = rawJoystickX - 512;
+    offsetY = rawJoystickY - 512;
+  }
+  return true;
+  
+}
+
+void getCalibratedJoystickValues() {
+
+  // Read the raw joystick values and apply a memory-efficient low-pass filter
+  // Note: There is another low-pass filter on the robot side, which is why
+  // this one is not very aggressive
+  rawJoystickX = (int16_t)((float)rawJoystickX * 0.75f + (float)analogRead(A0) * 0.25f);
+  rawJoystickY = (int16_t)((float)rawJoystickY * 0.75f + (float)analogRead(A1) * 0.25f);
+  
+  // Next, applied the offsets we got when calibrating the system, this helps
+  // to make sure the robot stops when the joystick is released
+  // This needs to be done separately so that the raw values can be used in the filter
+  // on the next loop
+  joystickX = rawJoystickX - offsetX;
+  joystickY = rawJoystickY - offsetY;
+  
+}
+
+void getButtonValues() {
+
+  // This is not cleaned up, that is dealt with on the robot side, where we have
+  // more processing power
+  // TODO: Fix this mess. Setting a byte value to 255 makes it return some garbage
+  // when read because a byte can't have a value of 255. This does not break anything
+  // because it's read as a uint8_t on the other side, but still...
+
+  // button0 is the joystick button, which outputs about 0.5V when pressed,
+  // no actually it does whatever the hell it wants to. Its literally different
+  // every time I log it to fix it because it always breaks.
+  // This time around its just following the x axis of the joystick and oscillates
+  // around it in some weird ways. Once I fix this and it inevitably breaks again,
+  // that will be it, because it will just follow the damn joystick precisely.
+  // "Quality second hand parts" my ass, this shit doesnt work in the slightest
+  // so it's read as an analog signal and the threshold is set to 50 / 1024
+  // yeah yeah threshold whatever. it has to have a low pass filter now, so
+  // if somebody presses it very rapidly, it wont activate. nice.
+  // it also has to be limited to when the joystick is centered, because it's getting
+  // worse and worse and every time I fix it, it breaks the next day again.
+  
+  button0analog = (button0analog * 0.2 + analogRead(A2) * 0.8);
+  button0 = (button0analog == 0 && joystickX == 512 && joystickY == 512) ? 255 : 0;
+  // One of these other two is open when pressed, the other is closed when pressed
+  button1 = digitalRead(2) ? 255 : 0;
+  button2 = digitalRead(3) ? 255 : 0;
+  
+}
+
+void checkForJoystickDeadspace() {
+  
+  // If values are close to mid-point, treat them as if they
+  // were exactly in the middle. This creates a dead zone, but makes
+  // sure that if the joystick is released, the robot stops
+  // This code is repeated on the robot side to really make sure
+  // the robot stops if we ask it to do that
+  
+  if (joystickX >= 496 && joystickX <= 525) joystickX = 512;
+  if (joystickY >= 496 && joystickY <= 525) joystickY = 512;
+  
+}
+
+void combineAndSendData() {
+
+  // vals saves the outgoing values as they are created
+  // they are then sent all at once
+
+  // Save all data into an array
+  byte vals[N_IDENTIFIERS] = { 
+    (byte)(joystickX), (byte)(joystickX >> 8),
+    (byte)(joystickY), (byte)(joystickY >> 8),
+    button0, button1, button2
+  };
+
+  // Send each identifier (a,b,c,...) followed
+  // by the value
+  for (int i = 0; i < N_IDENTIFIERS; i++) {
+    Serial.print(identifiers[i]);
+    Serial.write(vals[i]);
+  }
+  
 }
  
